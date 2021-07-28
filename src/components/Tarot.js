dc1cfbc88d4c540370f52fd303267eed531b4922
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-07-28 10:41:12 -0800

    add fortunes in meanings in unordered list

--- src/components/Tarot.js
+++ src/components/Tarot.js
@@ -4,14 +4,14 @@ import Card from "react-bootstrap/Card";
 
 
 function Tarot(props) {
-   const { name, fortuneTelling, lightMeaning, shadowMeaning } = props;
+   const { name } = props;
   const imageName = name.split(/[\s/]+/).join("");
   const [imageClicked, imageToggle] = useState(false);
  
   return (
     <>
-    <React.Fragment>
-      {/* {props.imageSelected ?
+      <React.Fragment>
+        {/* {props.imageSelected ?
         <Card>
           <Card.Text>
             <ul>
@@ -29,30 +29,45 @@ function Tarot(props) {
         </Card>
         :
        null */}
-      {/* };  */}
-      <Card>
-        <Card.Title>
-          <h3>{name.toUpperCase()}</h3>
-        </Card.Title>
-        <button onClick={() => imageToggle(!imageClicked)}>
-          <Card.Img
-            // onClick={onClickingImage}
-            src={require(`./../img/TarotImg/${imageName}.jpeg`)}
-          />
-        </button>
-        {imageClicked ? 
-        <div>
-          <h3>{ fortuneTelling }</h3> 
-          <h3><strong>Light Meaning:</strong> { lightMeaning }</h3>
-          <h3><strong>Shadow Meaning:</strong> { shadowMeaning }</h3>
-        </div>
-        : 
-        null
-      }
-      
-      </Card>
-
-    </React.Fragment>
+        {/* };  */}
+        <Card>
+          <Card.Title>
+            <h3>{name.toUpperCase()}</h3>
+          </Card.Title>
+          <button onClick={() => imageToggle(!imageClicked)}>
+            <Card.Img
+              // onClick={onClickingImage}
+              src={require(`./../img/TarotImg/${imageName}.jpeg`)}
+            />
+          </button>
+          {imageClicked ? (
+            <div>
+              <ul>
+                {props.fortuneTelling.map((fortune, index) => (
+                  <li key={index}>{fortune}</li>
+                ))}
+              </ul>
+              {/* <h3>{fortuneTelling}</h3> */}
+              <h5>
+                <strong>Light Meaning:</strong>
+              </h5>
+              <ul>
+                {props.lightMeaning.map((fortune, index) => (
+                  <li key={index}>{fortune}</li>
+                ))}
+              </ul>
+              <h5>
+                <strong>Shadow Meaning:</strong>
+              </h5>
+              <ul>
+                {props.shadowMeaning.map((fortune, index) => (
+                  <li key={index}>{fortune}</li>
+                ))}
+              </ul>
+            </div>
+          ) : null}
+        </Card>
+      </React.Fragment>
     </>
   );
 }
